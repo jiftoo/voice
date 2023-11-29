c148d6aea91a3ee1af5fa65cf13edee626669d33
Dan Kov <0x666c@mail.ru>
2023-11-29 06:21:53 +0300

    fix audio desync

--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -1,6 +1,12 @@
-use std::{fmt::Display, io, ops::Range, path::PathBuf, process::Stdio, sync::Arc, time::Duration};
+use std::{
+	fmt::Display, io, ops::Range, path::PathBuf, process::Stdio, sync::Arc,
+	time::Duration,
+};
 
-use tokio::{process::{Child, Command}, io::AsyncWriteExt,};
+use tokio::{
+	io::AsyncWriteExt,
+	process::{Child, Command},
+};
 
 pub struct FFmpeg {
 	input: PathBuf,
@@ -22,18 +28,22 @@ impl OutputParser {
 		match self {
 			OutputParser::Start => Ok((
 				OutputParser::End(
-					Self::parse_line(line, "start")?.ok_or(io::Error::new(io::ErrorKind::InvalidData, line))?,
+					Self::parse_line(line, "start")?
+						.ok_or(io::Error::new(io::ErrorKind::InvalidData, line))?,
 				),
 				None,
 			)),
 			OutputParser::End(start) => Ok((
 				OutputParser::Duration(
 					start,
-					Self::parse_line(line, "end")?.ok_or(io::Error::new(io::ErrorKind::InvalidData, line))?,
+					Self::parse_line(line, "end")?
+						.ok_or(io::Error::new(io::ErrorKind::InvalidData, line))?,
 				),
 				None,
 			)),
-			OutputParser::Duration(start, end) => Ok((OutputParser::Start, Some(start..end))),
+			OutputParser::Duration(start, end) => {
+				Ok((OutputParser::Start, Some(start..end)))
+			}
 		}
 	}
 
@@ -127,8 +137,10 @@ impl FFmpeg {
 			// coded in epic hurry
 			let str = "Duration: ";
 			let i = stderr_text.find(str).unwrap();
-			let split = stderr_text.split_at(i + str.len()).1.split_once(',').unwrap().0.trim();
-			let split = split.split(':').map(|x| x.parse::<f32>().unwrap()).collect::<Vec<_>>();
+			let split =
+				stderr_text.split_at(i + str.len()).1.split_once(',').unwrap().0.trim();
+			let split =
+				split.split(':').map(|x| x.parse::<f32>().unwrap()).collect::<Vec<_>>();
 			Duration::from_secs_f32(split[0] * 3600.0 + split[1] * 60.0 + split[2])
 		};
 
@@ -152,9 +164,15 @@ impl FFmpeg {
 		Ok(VideoAnalysis::new(ranges, video_duration))
 	}
 
-	pub async fn spawn_remove_silence(&self, keep_fragments: &[Range<f32>]) -> io::Result<Child> {
+	pub async fn spawn_remove_silence(
+		&self,
+		keep_fragments: &[Range<f32>],
+	) -> io::Result<Child> {
 		if keep_fragments.is_empty() {
-			return Err(io::Error::new(io::ErrorKind::InvalidInput, "no fragments to keep"));
+			return Err(io::Error::new(
+				io::ErrorKind::InvalidInput,
+				"no fragments to keep",
+			));
 		}
 		let filter = keep_fragments
 			.iter()
@@ -162,8 +180,22 @@ impl FFmpeg {
 			.reduce(|a, b| format!("{}+{}", a, b))
 			.unwrap();
 
-		let vf = format!("select='{filter}',setpts=N/FRAME_RATE/TB,scale='trunc(oh*a/2)*2:576'");
-		let af = format!("aselect='{filter}',asetpts=N/SR/TB");
+		let remove_fragments = keep_fragments
+			.windows(2)
+			.map(|ab| ab[0].end..ab[1].start)
+			.collect::<Vec<_>>();
+
+		let pts_shifts = remove_fragments
+			.into_iter()
+			.map(|x| format!("gt(T,{})*({})", x.start, x.end - x.start))
+			.reduce(|a, b| format!("{}+{}", a, b))
+			.unwrap();
+
+		let pts_expr = format!("PTS-STARTPTS-({pts_shifts})/TB",);
+
+		let vf =
+			format!("select='{filter}',setpts='{pts_expr}',scale='trunc(oh*a/2)*2:576'");
+		let af = format!("aselect='{filter}',asetpts='{pts_expr}'");
 
 		let filter_complex = format!("[0:v]{vf}[video];[0:a]{af}[audio]");
 
