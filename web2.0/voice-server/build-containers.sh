e90b392c7c18dc869b0bfbaa688adee748eeedd1
Dan Kov <0x666c@mail.ru>
2024-02-24 16:02:12 +0300

    squash fix

--- web2.0/voice-server/build-containers.sh
+++ web2.0/voice-server/build-containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Building voice-waveform-gen
-docker build -t voice-waveform-gen --target voice-wave-gen .
+docker build -t voice-waveform-gen --target voice-waveform-gen .
 echo Building voice-analyzer
 docker build -t voice-waveform-gen --target voice-analyzer .
 echo Building voice-server
