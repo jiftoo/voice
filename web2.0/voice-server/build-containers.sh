97190493d1bcb660f11fc9501525b7714ce813df
Dan Kov <0x666c@mail.ru>
2024-02-24 16:01:50 +0300

    fix script

--- web2.0/voice-server/build-containers.sh
+++ web2.0/voice-server/build-containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Building voice-waveform-gen
-docker build -t voice-waveform-gen --target voice-wave-gen
+docker build -t voice-waveform-gen --target voice-wave-gen .
 echo Building voice-analyzer
-docker build -t voice-waveform-gen --target voice-analyzer
+docker build -t voice-waveform-gen --target voice-analyzer .
 echo Building voice-server
-docker build -t voice-waveform-gen --target voice-waveform-gen
+docker build -t voice-waveform-gen --target voice-waveform-gen .
