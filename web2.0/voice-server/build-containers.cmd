582051ff7708110bc4c986cc0d8065ee59439f23
Dan Kov <0x666c@mail.ru>
2024-09-11 02:20:23 +0300

    dockerfile for uri service

--- web2.0/voice-server/build-containers.cmd
+++ web2.0/voice-server/build-containers.cmd
@@ -3,6 +3,8 @@ echo Building voice-analyzer
 docker build -t voice-analyzer --target voice-analyzer .
 echo Building voice-waveform-gen
 docker build -t voice-waveform-gen --target voice-waveform-gen .
+echo Building voice-uri-upload
+docker build -t voice-uri-upload --target voice-uri-upload .
 
 echo Pushing
 docker tag voice-analyzer cr.yandex/crpkd0jf7p97brcjvubn/voice-analyzer
@@ -10,3 +12,6 @@ docker push cr.yandex/crpkd0jf7p97brcjvubn/voice-analyzer
 
 docker tag voice-waveform-gen cr.yandex/crpkd0jf7p97brcjvubn/voice-waveform-gen
 docker push cr.yandex/crpkd0jf7p97brcjvubn/voice-waveform-gen
+
+docker tag voice-uri-upload cr.yandex/crpkd0jf7p97brcjvubn/voice-uri-upload
+docker push cr.yandex/crpkd0jf7p97brcjvubn/voice-uri-upload
